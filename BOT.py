69606784c77bb4730e6781fceeb4d4c0d9a30ec2
Andrea055 <vivanco11.ac@gmail.com>
2021-05-23 00:30:06 +0200

    Remove negative filter and some fix
    negative doesn't work and crash bot

--- BOT.py
+++ BOT.py
@@ -1,7 +1,6 @@
 import argparse as arg
 import sys
 import cv2
-import blacknwhite_filter
 import discord                                          #import libraries
 from discord.ext import commands
 from discord.ext.commands import bot
@@ -15,13 +14,18 @@ from PIL import Image
 import time
 import os
 import sys
+import blacknwhite_filter
 import cartoon_filter
+import sketch_filter
 import vintage_filter
+import cool_filter
 
-client = commands.Bot(command_prefix=".") 
 bnw = blacknwhite_filter.BlacknWhite()
 crt = cartoon_filter.Cartoonizer()
+skch = sketch_filter.Sketcher()
 vntg = vintage_filter.Vintage()
+cool = cool_filter.Cool()
+client = commands.Bot(command_prefix=".")   
 
 
 
@@ -29,6 +33,22 @@ vntg = vintage_filter.Vintage()
 async def on_ready():
     print("Bot is ready")
 
+@client.command()                                       # blackwhite command and processing 
+async def sketch(ctx):
+    try:
+        url = ctx.message.attachments[0].url            # check for an image, call exception if none found
+    except IndexError:
+        print("Error: No attachments")                  # check attachments presence
+        await ctx.send("No attachments detected!")
+    else:
+        if url[0:26] == "https://cdn.discordapp.com":   # look to see if url is from discord
+            r = requests.get(url, stream=True)
+            imageName = 'noskch' + '.png'                   # save bw image
+            with open(imageName, 'wb') as out_file:
+                print('Saving image: ' + imageName)
+                shutil.copyfileobj(r.raw, out_file)
+    skch.start('noskch.png')
+    await ctx.send('Cartoon!', file=discord.File('Sketch version.jpg'))
 
 @client.command()                                       # blackwhite command and processing 
 async def cartoon(ctx):
@@ -80,6 +100,23 @@ async def vintage(ctx):
                 shutil.copyfileobj(r.raw, out_file)
     vntg.start('novintage.png')
     await ctx.send('Vintage!', file=discord.File('Vintage_version.jpg'))
+
+@client.command()                                       # blackwhite command and processing 
+async def cool(ctx):
+    try:
+        url = ctx.message.attachments[0].url            # check for an image, call exception if none found
+    except IndexError:
+        print("Error: No attachments")                  # check attachments presence
+        await ctx.send("No attachments detected!")
+    else:
+        if url[0:26] == "https://cdn.discordapp.com":   # look to see if url is from discord
+            r = requests.get(url, stream=True)
+            imageName = 'nocool' + '.png'                   # save bw image
+            with open(imageName, 'wb') as out_file:
+                print('Saving image: ' + imageName)
+                shutil.copyfileobj(r.raw, out_file)
+    vntg.start('nocool.png')
+    await ctx.send('Vintage!', file=discord.File('Cool_version.jpg'))
 		
-client.run('TOKEN')
+client.run('ODQ1MzYzNDQ1Nzk2NzAwMTgw.YKf4Dw.CJpuSnhSAyYFW4hg08eCOt-GJMw')
 		
